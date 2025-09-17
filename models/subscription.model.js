836387eb4a114c20e81490d5cf0eff144f83332a
sumitsasidharan <s.sumit196@gmail.com>
2025-09-17 21:46:47 +0530

    upded sub model

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,28 +1,80 @@
 import mongoose from 'mongoose';
 
-const subscriptionSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Subscription name is required'],
-    trim: true,
-    minLength: 2,
-    maxLength: 100,
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Subscription name is required'],
+      trim: true,
+      minLength: 2,
+      maxLength: 100,
+    },
+    price: {
+      type: Number,
+      required: [true, 'Subscription price is required'],
+      min: [0, 'Price must be greater than 0'],
+    },
+    currency: {
+      type: String,
+      enum: ['USD', 'EUR', 'GBP'],
+      default: 'USD',
+    },
+    frequency: {
+      type: String,
+      enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    },
+    category: {
+      type: String,
+      enum: [
+        'sports',
+        'news',
+        'entertainment',
+        'lifestyle',
+        'technology',
+        'finance',
+        'politics',
+      ],
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ['active', 'cancelled', 'expired'],
+      default: 'active',
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value <= new Date()
+        },
+        message: 'Start date must  be in the past',
+      },
+    },
+    renewalDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+        message: 'Renewal date must be after the start date',
+      },
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      index: true
+    }
   },
-  price: {
-    type: Number,
-    required: [true, 'Subscription price is required'],
-    min: [0, 'Price must be greater than 0'],
-  },
-  currency: {
-    type: String,
-    enum: ['USD', 'EUR', 'GBP'],
-    default: 'USD'
-  },
-  frequency: {
-    type: String,
-    enum: ['daily', 'weekly', 'monthly', 'yearly']
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Subscription = mongoose.model('Subscription', subscriptionSchema);
 
